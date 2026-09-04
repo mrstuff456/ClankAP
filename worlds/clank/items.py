15f638aaa6870e267101f5973cc5a6a74f14974b
mrstuff456 <115061061+mrstuff456@users.noreply.github.com>
2026-09-04 21:59:31 +1200

    SQLite Items
    changed items to pull from an SQLite database, and added item support for card packs

--- worlds/clank/items.py
+++ worlds/clank/items.py
@@ -7,29 +7,40 @@ from BaseClasses import Item, ItemClassification
 if TYPE_CHECKING:
     from .world import ClankWorld
 
-
-ITEM_NAME_TO_ID = {
-    #"Artifact Unlock: Bracelet": 1,
-    "Artifact Unlock: Anhk": 2,
-    "Artifact Unlock: Urn": 3,
-    "Artifact Unlock: Golden Banana": 4,
-    "Artifact Unlock: Shield": 5,
-    "Artifact Unlock: Chestplate": 6,
-    "Artifact Unlock: Golden Treasure": 7,
-    "filler": 8,
-}
+import sqlite3
+# init SQLite database
+con = sqlite3.connect("ClankDatabase.db")
+cur = con.cursor()
 
 
-DEFAULT_ITEM_CLASSIFICATIONS = {
-    #"Artifact Unlock: Bracelet": ItemClassification.progression,
-    "Artifact Unlock: Anhk": ItemClassification.progression,
-    "Artifact Unlock: Urn": ItemClassification.progression,
-    "Artifact Unlock: Golden Banana": ItemClassification.progression,
-    "Artifact Unlock: Shield": ItemClassification.progression,
-    "Artifact Unlock: Chestplate": ItemClassification.progression,
-    "Artifact Unlock: Golden Treasure": ItemClassification.progression,
-    "filler": ItemClassification.filler,
-}
+ITEM_NAME_TO_ID = {}
+DEFAULT_ITEM_CLASSIFICATIONS = {}
+
+
+# add all the items to the relative dictionaries
+def init_db():
+    # get everything from the database
+    cur.execute("""
+    SELECT i.ID, i.Name, c.ClassificationName FROM Item i 
+    INNER JOIN ItemClassification c ON c.ID = i.Classification
+    """)
+    itemData = cur.fetchall()
+
+    # add the database data to the dictionaries
+    for i in itemData:
+        ITEM_NAME_TO_ID[i[1]] = i[0] # add to item dictionary: "name": id
+        DEFAULT_ITEM_CLASSIFICATIONS[i[1]] = ItemClassification[i[2]] # add to classification dict: "name": "classification"
+
+
+
+# helper function for gaining data from database for option-specific checks
+def get_db_data_by_type(typeQuery):
+    cur.execute(f"""
+        SELECT Name FROM Item WHERE Type IN(
+            SELECT ID FROM ItemType WHERE Name = '{typeQuery}')
+        """)
+    resultData = cur.fetchall()
+    return resultData
 
 
 class ClankItem(Item):
@@ -48,15 +59,34 @@ def create_item_with_correct_classification(world: ClankWorld, name: str) -> Cla
 
 def create_all_items(world: ClankWorld) -> None:
 
-    # create the pool containing all the items
-    itempool: list[Item] = [
-        world.create_item("Artifact Unlock: Anhk"),
-        world.create_item("Artifact Unlock: Urn"),
-        world.create_item("Artifact Unlock: Golden Banana"),
-        world.create_item("Artifact Unlock: Shield"),
-        world.create_item("Artifact Unlock: Chestplate"),
-        world.create_item("Artifact Unlock: Golden Treasure"),
-    ]
+    # init db
+    init_db()
+
+    # create the pool and items to add lists
+    itempool: list[Item] = []
+    ITEMS_TO_ADD = []
+
+    # Create all the items needed
+
+    # Artifact Unlocks:
+    if world.multiworld.options.artifacts:
+        artifactNames = get_db_data_by_type("artifact_unlock")
+        for i in artifactNames:
+            if i not in ["Artifact Unlock: Bracelet", "Artifact Unlock: Anhk"]:
+                ITEMS_TO_ADD.append(i)
+
+
+    # Dungeon Row Unlocks:
+    if world.multiworld.options.dungeon_row:
+        if world.multiworld.options.row_shuffling_style == "Packs":
+            packNames = get_db_data_by_type("dungeon_row_pack")
+            for i in packNames:
+                ITEMS_TO_ADD.append(i)
+
+
+    # insert all the items into the pool
+    for i in ITEMS_TO_ADD:
+        itempool.append(world.create_item(i))
 
 
     # get number of required filler items
